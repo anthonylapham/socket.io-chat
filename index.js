0eb0ee22f799d3408c72665a9bc9dbd373c2e3be
Anthony Lapham <lapham.2@wright.edu>
2018-10-14 09:54:24 -0400

    chat updates

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
-  res.send('<h1>Hello World</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) =>{
+  console.log('A user connected');
 });
 
 http.listen(3000, function(){
