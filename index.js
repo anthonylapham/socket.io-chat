21eabff504c21cea1212b4e6962fc6dd6307d29b
Anthony Lapham <lapham.2@wright.edu>
2018-10-15 19:22:51 -0400

    first phase complete

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
   console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnect');
+  })
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
