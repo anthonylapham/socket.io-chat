201a4eed355c2746f4189b803686050e165b7f1b
Anthony Lapham <lapham.2@wright.edu>
2018-10-20 20:10:20 -0400

    working on typing functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const people = {};
 
 
 app.use(express.static(__dirname + '/public'));
@@ -11,15 +12,25 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+//broadcast to users that someone is typing
+io.on('is typing', function(data){
+  io.emit('typing');
+});
+
 io.on('connection', (socket) => {
   socket.on('chat message', function(msg) {
+    //people[client.id] = name;
     io.emit('chat message', msg);
+    //socket.sockets.emit('update,' + name + 'has joined!');
   });
   console.log('A user connected');
   socket.on('disconnect', () => {
+    //socket.sockets.emit('update,' + name + 'has left the chat');
     console.log('User disconnect');
-  })
+  });
 });
+
+
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
